666902ae0dc3ce9d5b67e676b1e0c15110ed21ab
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2023-08-23 22:24:01 -0300

    feat: config layout and add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+import Image from 'next/image'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +22,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.className} bg-gray1 pt-[6.25rem]`}>
+        <main className="flex min-h-screen flex-col">
+          <div className="bg-gray2 fixed top-0 h-24 w-full">
+            <div className="mx-auto flex h-full w-full max-w-screen-lg items-center justify-center">
+              <Image
+                src="/logo.svg"
+                width={208}
+                height={60}
+                alt="Ignite Feed"
+              />
+            </div>
+          </div>
+
+          <div className="mx-auto mt-8 h-10 w-full max-w-screen-lg ">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
